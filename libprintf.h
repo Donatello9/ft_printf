842494530237625faa6ca10d48bbfe6ce05bc94b
Sofian Iligh <siligh@made-f0Dr5s2.clusters.42paris.fr>
2024-06-07 17:14:11 +0200

    07/06/2024

---
+++ libprintf.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libprintf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: siligh <siligh@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/07 14:36:01 by siligh            #+#    #+#             */
+/*   Updated: 2024/06/07 15:02:35 by siligh           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBPRINTF_H
+# define LIBPRINTF_H
+# include <stdarg.h>
+# include <stddef.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+void	percent_c(char c);
+void	percent_s(char *str);
+void	percent_p(void *hex);
+void	percent_d(double n);
+void	percent_i(int n);
+void	percent_u(unsigned int nb);
+void	percent_x(int hex);
+void	percent_X(int hex);
+void	percentPERCENT(int c);
+
+#endif
